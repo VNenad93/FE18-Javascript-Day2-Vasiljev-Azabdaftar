d54de012adf1676659bf5cbe80161fa0ad18a19e
Nazila <azabdaftar@outlook.com>
2023-02-02 13:37:02 +0100

    first letter

---
+++ Nazila/Intermediate/Capitalize/js/js/script.js
@@ -0,0 +1,4 @@
+(function(txtSample) {
+    let letter = txtSample.charAt(0);
+    console.log(letter);
+})("If we pass the following string 'i am a web developer' we should get the message 'I am a web developer");
