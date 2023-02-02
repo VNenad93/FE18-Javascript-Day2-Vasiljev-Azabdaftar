c3d075334029f3871b2684c55193e163ff2e7c48
Nazila <azabdaftar@outlook.com>
2023-02-02 12:15:12 +0100

    ex3

---
+++ Nazila/Basic/EX 3/js/script.js
@@ -0,0 +1,11 @@
+let childNo = 2;
+let pName = "Hanah";
+let geoLocation = "Austria";
+let jobTitle = "Ing.";
+
+
+function crystalGazer(childNo, pName, geoLocation, jobTitle) {
+    return `You will be a ${jobTitle} in ${geoLocation} and married to ${pName} with ${childNo}.`
+}
+
+document.write(crystalGazer(2, "Hanah", "Austria", "Ing."));
